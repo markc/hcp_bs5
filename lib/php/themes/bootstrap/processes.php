0075a2fae3096d73bcaca84fa49f32dd280890f0
Mark Constable <markc@renta.net>
2018-04-05 16:09:32 +1000

    Second part of splitting processes from infosys

--- lib/php/themes/bootstrap/processes.php
+++ lib/php/themes/bootstrap/processes.php
@@ -1,24 +1,22 @@
 <?php
-// lib/php/themes/bootstrap/infosys.php 20170225 - 20170513
-// Copyright (C) 2015-2017 Mark Constable <markc@renta.net> (AGPL-3.0)
+// lib/php/themes/bootstrap/processes.php 20170225 - 20180405
+// Copyright (C) 2015-2018 Mark Constable <markc@renta.net> (AGPL-3.0)
 
-class Themes_Bootstrap_InfoSys extends Themes_Bootstrap_Theme
+class Themes_Bootstrap_Processes extends Themes_Bootstrap_Theme
 {
     public function list(array $in) : string
     {
 error_log(__METHOD__);
 
-error_log(var_export($in,true));
-
         extract($in);
 
         return '
-          <div class="col-6">
-            <h3><i class="fas fa-server fa-fw"></i> System Info</h3>
+          <div class="col-12 col-sm-6">
+            <h3><i class="fas fa-code-branch fa-fw"></i> Processes</h3>
           </div>
-          <div class="col-6">
+          <div class="col-12 col-sm-6">
             <form method="post" class="form-inline">
-              <input type="hidden" id="o" name="o" value="infosys">
+              <input type="hidden" id="o" name="o" value="processes">
               <div class="form-group ml-auto">
                 <button type="submit" class="btn btn-primary"><i class="fas fa-sync-alt fa-fw" aria-hidden="true"></i> Refresh</button>
               </div>
@@ -26,72 +24,10 @@ error_log(var_export($in,true));
           </div>
         </div><!-- END UPPER ROW -->
         <div class="row">
-          <div class="col-6">
-            <br>
-            <h5>RAM <small>' . $mem_used . ' / ' . $mem_total . ', ' . $mem_free . ' free</small></h5>
-            <div class="progress">
-              <div class="progress-bar bg-' . $mem_color . '" role="progressbar" aria-valuenow="' . $mem_pcnt . '"
-              aria-valuemin="0" aria-valuemax="100" style="width:' . $mem_pcnt . '%" title="Used Memory">' . $mem_text . '
-              </div>
-            </div>
-            <br>
-            <h5>Disk <small>' . $dsk_used . ' / ' . $dsk_total . ', ' . $dsk_free . ' free</small></h5>
-            <div class="progress">
-              <div class="progress-bar bg-' . $dsk_color . '" role="progressbar" aria-valuenow="' . $dsk_pcnt . '"
-              aria-valuemin="0" aria-valuemax="100" style="width:' . $dsk_pcnt . '%" title="Used Disk Space">' . $dsk_text . '
-              </div>
-            </div>
-            <br>
-            <h5>CPU <small>' .$cpu_all  . '</small></h5>
-            <div class="progress">
-              <div class="progress-bar bg-' . $cpu_color . '" role="progressbar" aria-valuenow="' . $cpu_pcnt . '"
-              aria-valuemin="0" aria-valuemax="100" style="width:' . $cpu_pcnt . '%" title="Used Disk Space">' . $cpu_text . '
-              </div>
-            </div>
-            <br>
-          </div>
-          <div class="col-6">
-            <div class="table-responsive">
-              <table class="table table-sm table-bordered">
-                <tbody>
-                  <tr>
-                    <td><b>Hostname</b></td>
-                    <td>' .$hostname  . '</td>
-                  </tr>
-                  <tr>
-                    <td><b>Host IP</b></td>
-                    <td>' . $host_ip . '</td>
-                  </tr>
-                  <tr>
-                    <td><b>Distro</b></td>
-                    <td>' . $os_name . '</td>
-                  </tr>
-                  <tr>
-                    <td><b>Uptime</b></td>
-                    <td>' . $uptime . '</td>
-                  </tr>
-                  <tr>
-                    <td><b>CPU Load</b></td>
-                    <td>' . $loadav . ' (' . $cpu_num . ' cpus)</td>
-                  </tr>
-                  <tr>
-                    <td><b>CPU Model</b></td>
-                    <td>' . $cpu_name . '</td>
-                  </tr>
-                  <tr>
-                    <td><b>Kernel Version</b></td>
-                    <td>' . $kernel . '</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <div class="row">
           <div class="col-12">
             <h5>Process List <small>(' . $proc_num . ')</small></h5>
-            <pre>' . $proc_list . '
-            </pre>
+            <pre><code>' . $proc_list . '
+            </code></pre>
           </div>
         </div>';
     }
