f12197f4af29f90a985c4568d6c8991517210e8b
Mark Constable <markc@renta.net>
2018-05-14 21:12:02 +1000

    dkim module: Add rudimentary list and create functions

--- lib/php/plugins/dkim.php
+++ lib/php/plugins/dkim.php
@@ -4,14 +4,41 @@
 
 class Plugins_Dkim extends Plugin
 {
+    protected
+    $in = [
+        'domain'    => '',
+        'selector'  => 'dkim',
+        'bits'      => '1024',
+    ];
+
+    public function create() : string
+    {
+error_log(__METHOD__);
+
+        if ($_POST) {
+error_log('this->in='.var_export($this->in,true));
+            // check if domain is in vhosts
+            extract($this->in);
+            exec("sudo dkim add $selector $domain $bits 2>&1", $retArr, $retVal);
+            util::log('<pre>' . trim(implode("\n", $retArr)) . '</pre>', $retVal ? 'danger' : 'success');
+        }
+        return $this->list();
+    }
+
     public function list() : string
     {
 error_log(__METHOD__);
 
-        $retArr = []; $retVal = null;
+//        $retArr = []; $retVal = null;
         exec("sudo dkim show 2>&1", $retArr, $retVal);
-//        util::log('<pre>' . trim(implode("\n", $retArr)) . '</pre>', $retVal ? 'danger' : 'success');
-        return $this->t->list(['buf' =>  trim(implode("\n", $retArr))]);
+        $buf = '';
+        $cnt = count($retArr);
+        for($i = 0; $i < $cnt; $i++) {
+            $buf .= ($i % 2 == 0) ? '
+        <b>'.$retArr[$i].'</b><br>' : '
+        <div style="word-break:break-all;font-family:monospace;width:100%;">' . $retArr[$i] . '</div><hr>';
+        }
+        return $this->t->list(['buf' => $buf]);
     }
 
 }
--- lib/php/themes/bootstrap/dkim.php
+++ lib/php/themes/bootstrap/dkim.php
@@ -1,5 +1,5 @@
 <?php
-// lib/php/themes/bootstrap/dkim.php 20180511 - 20180511
+// lib/php/themes/bootstrap/dkim.php 20180511 - 20180514
 // Copyright (C) 2015-2018 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Themes_Bootstrap_Dkim extends Themes_Bootstrap_Theme
@@ -8,23 +8,68 @@ class Themes_Bootstrap_Dkim extends Themes_Bootstrap_Theme
     {
 error_log(__METHOD__);
 
-       return '
-        <div class="col-12">
-          <h3>
-            <i class="fas fa-address-card fa-fw"></i> DKIM
-            <a href="#" title="Add New DKIM" data-toggle="modal" data-target="#createmodal">
-              <small><i class="fas fa-plus-circle fa-fw"></i></small>
-            </a>
-          </h3>
+        $dkim_length_buf = $this->dropdown([
+            ['1024', '1024'],
+            ['2048', '2048'],
+        ], 'bits', '', '', 'custom-select');
+
+        return '
+            <div class="col-12">
+              <h3>
+                <i class="fas fa-address-card fa-fw"></i> DKIM
+                <a href="#" title="Add New DKIM" data-toggle="modal" data-target="#createmodal">
+                  <small><i class="fas fa-plus-circle fa-fw"></i></small>
+                </a>
+              </h3>
+            </div>
+          </div><!-- END UPPER ROW -->
+          <div class="row">
+            <div class="col-12">' . $in['buf'] . '
+            </div>
+          </div>
         </div>
-      </div><!-- END UPPER ROW -->
-      <div class="row">
-        <div class="col-12">
-          <p>DKIM records will appear here... </p>
-          <pre>' . $in['buf'] . '</pre>
+        <div class="modal fade" id="createmodal" tabindex="-1" role="dialog" aria-labelledby="createmodal" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Add DKIM</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form method="post" action="' . $this->g->cfg['self'] . '">
+                <input type="hidden" name="c" value="' . $_SESSION['c'] . '">
+                <input type="hidden" name="o" value="' . $this->g->in['o'] . '">
+                <input type="hidden" name="i" value="' . $this->g->in['i'] . '">
+                <input type="hidden" name="m" value="create">
+                <div class="modal-body">
+                  <div class="form-group">
+                    <label for="user" class="form-control-label">Domain</label>
+                    <input type="text" class="form-control" id="domain" name="domain">
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="form-group">
+                        <label for="selector" class="form-control-label">Selector</label>
+                        <input type="text" class="form-control" id="selector" name="selector" value="dkim">
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div class="form-group">
+                        <label for="bits" class="form-control-label">Bit Length</label>' . $dkim_length_buf . '
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Add New DKIM Record</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>';
+    ';
     }
 }
 
