fe055c33f8caeca61b738291e105146b0d71f0c1
Mark Constable <markc@renta.net>
2020-08-08 16:43:24 +1000

    plugins/infosys.php : let's try + Shmem

--- lib/php/plugins/infosys.php
+++ lib/php/plugins/infosys.php
@@ -58,7 +58,7 @@ elog(__METHOD__);
         $dp  = floor(($du / $dt) * 100);
 
         $mt  = (float) $mem['MemTotal'] * 1024;
-        $mf  = (float) ($mem['MemFree'] + $mem['Cached'] + $mem['Buffers'] + $mem['SReclaimable']) * 1024;
+        $mf  = (float) ($mem['MemFree'] + $mem['Cached'] + $mem['Buffers'] + $mem['SReclaimable'] + $mem['Shmem']) * 1024;
         $mu  = (float) $mt - $mf;
         $mp  = floor(($mu / $mt) * 100);
 
