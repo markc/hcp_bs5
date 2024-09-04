6cf6422349048130b270cec79e1503053c1818c5
Mark Constable <markc@renta.net>
2024-09-04 15:50:21 +1000

    Refactored to PHP 8.3

--- lib/php/plugins/processes.php
+++ lib/php/plugins/processes.php
@@ -1,13 +1,14 @@
 <?php
 
 declare(strict_types=1);
-// plugins/processes.php 20170225 - 20180430
-// Copyright (C) 2015-2018 Mark Constable <markc@renta.net> (AGPL-3.0)
+
+// plugins/processes.php 20170225 - 20240904
+// Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Plugins_Processes extends Plugin
 {
     public function list(): string
     {
-        return $this->g->t->list(['procs' => shell_exec('sudo processes')]);
+        return $this->g->t->list(['procs' => shell_exec('sudo processes') ?: '']);
     }
 }
