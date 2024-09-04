19f1175c68307cf15ce8fbeb77ab3b809360c58b
Mark Constable <markc@renta.net>
2024-09-04 15:33:59 +1000

    Refactored to PHP 8.3 and removed comments

--- lib/php/plugins/home.php
+++ lib/php/plugins/home.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-// lib/php/plugins/home.php 20150101 - 20180614
-// Copyright (C) 2015-2018 Mark Constable <markc@renta.net> (AGPL-3.0)
+
+// lib/php/plugins/home.php 20150101 - 20240904
+// Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Plugins_Home extends Plugin
 {
@@ -11,9 +12,11 @@ class Plugins_Home extends Plugin
     public function list(): string
     {
         if (file_exists(INC . 'home.tpl')) {
-            ob_start();
-            include INC . 'home.tpl';
-            return ob_get_clean();
+            return (function () {
+                ob_start();
+                include INC . 'home.tpl';
+                return ob_get_clean();
+            })();
         }
 
         return $this->g->t->list([]);
