79e1cae93d3a9a2a06eca677b1aaedb86177c3a3
Mark Constable <markc@renta.net>
2024-09-04 16:50:59 +1000

    Refactor to PHP 8..3 and remove comments

--- lib/php/themes/bootstrap/accounts.php
+++ lib/php/themes/bootstrap/accounts.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-// lib/php/themes/bootstrap/accounts.php 20170225 - 20230623
-// Copyright (C) 2015-2023 Mark Constable <markc@renta.net> (AGPL-3.0)
+
+// lib/php/themes/bootstrap/accounts.php 20170225 - 20240904
+// Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Themes_Bootstrap_Accounts extends Themes_Bootstrap_Theme
 {
@@ -37,16 +38,14 @@ class Themes_Bootstrap_Accounts extends Themes_Bootstrap_Theme
             'action'    => 'delete',
             'lhs_cmd'   => '',
             'rhs_cmd'   => 'Remove',
-            'hidden'    => '
-            <input type="hidden" name="i" value="' . $this->g->in['i'] . '">',
-            'body'      => '
-            <p class="text-center">Are you sure you want to remove this user?<br><b>' . $usr['login'] . '</b></p>',
+            'hidden'    => sprintf('<input type="hidden" name="i" value="%s">', $this->g->in['i']),
+            'body'      => sprintf('<p class="text-center">Are you sure you want to remove this user?<br><b>%s</b></p>', $usr['login']),
         ]);
     }
 
     public function list(array $in): string
     {
-        return '
+        return <<<HTML
         <div class="row">
           <h3>
             <i class="bi bi-people-fill"></i> Accounts
@@ -84,86 +83,76 @@ class Themes_Bootstrap_Accounts extends Themes_Bootstrap_Theme
           </div>
         </div>
         <script>
-$(document).ready(function() {
-  $("#accounts").DataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": "?x=json&o=accounts&m=list",
-    "scrollX": true,
-    "columnDefs": [
-      {"targets":0, "className":"text-truncate"},
-      {"targets":3, "className":"text-truncate"},
-    ]
-  });
+        $(document).ready(function() {
+          $("#accounts").DataTable({
+            "processing": true,
+            "serverSide": true,
+            "ajax": "?x=json&o=accounts&m=list",
+            "scrollX": true,
+            "columnDefs": [
+              {"targets":0, "className":"text-truncate"},
+              {"targets":3, "className":"text-truncate"},
+            ]
+          });
 
-  $(document).on("click", ".bslink", function(){
-    event.preventDefault();
-    var url = $(this).attr("href") + "&x=html";
-    var m = new URLSearchParams(url).get("m");
-    $("#" + m + "dialog").load(url, function() {
-      $("#" + m + "modal", document).modal("show");
-    });
-  });
-
-});
-        </script>';
+          $(document).on("click", ".bslink", function(event){
+            event.preventDefault();
+            var url = $(this).attr("href") + "&x=html";
+            var m = new URLSearchParams(url).get("m");
+            $("#" + m + "dialog").load(url, function() {
+              $("#" + m + "modal", document).modal("show");
+            });
+          });
+        });
+        </script>
+        HTML;
     }
 
     private function modal_body(array $in): string
     {
-        $aclgrp_buf = '';
-        $acl_ary = $grp_ary = [];
-
         $acl = $_SESSION['usr']['acl'];
         $grp = $_SESSION['usr']['grp'];
 
-        foreach ($this->g->acl as $k => $v) {
-            $acl_ary[] = [$v, $k];
-        }
-
+        $acl_ary = array_map(fn($k, $v) => [$v, $k], array_keys($this->g->acl), $this->g->acl);
         $acl_buf = $this->dropdown($acl_ary, 'acl', "{$acl}", '', 'form-select');
 
-        $res = db::qry('
- SELECT login,id
-   FROM `accounts`
-  WHERE acl = :0 OR acl = :1', ['0' => 0, '1' => 1]);
-
-        foreach ($res as $k => $v) {
-            $grp_ary[] = [$v['login'], $v['id']];
-        }
-
+        $res = db::qry('SELECT login, id FROM `accounts` WHERE acl IN (0, 1)');
+        $grp_ary = array_map(fn($row) => [$row['login'], $row['id']], $res);
         $grp_buf = $this->dropdown($grp_ary, 'grp', "{$grp}", '', 'form-select');
 
-        $aclgrp_buf = '
+        $aclgrp_buf = <<<HTML
         <div class="row">
           <div class="col-6 mb-3">
-            <label for="acl" class="form-label">ACL</label>' . $acl_buf . '
+            <label for="acl" class="form-label">ACL</label>$acl_buf
           </div>
           <div class="col-6 mb-3">
-            <label for="grp" class="form-label">Group</label>' . $grp_buf . '
+            <label for="grp" class="form-label">Group</label>$grp_buf
           </div>
-        </div>';
+        </div>
+        HTML;
 
-        return '
+        return <<<HTML
         <div class="row">
           <div class="col-6 mb-3">
             <label for="login" class="form-label">Email ID</label>
-            <input type="email" class="form-control" id="login" name="login" value="' . $in['login'] . '" required>
+            <input type="email" class="form-control" id="login" name="login" value="{$in['login']}" required>
           </div>
           <div class="col-6 mb-3">
             <label for="altemail" class="form-label">Alt Email</label>
-            <input type="text" class="form-control" id="altemail" name="altemail" value=" ' . $in['altemail'] . '">
+            <input type="text" class="form-control" id="altemail" name="altemail" value="{$in['altemail']}">
           </div>
         </div>
         <div class="row">
           <div class="col-6 mb-3">
             <label for="fname" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="fname" name="fname" value="' . $in['fname'] . '" required>
+            <input type="text" class="form-control" id="fname" name="fname" value="{$in['fname']}" required>
           </div>
           <div class="col-6 mb-3">
             <label for="lname" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="lname" name="lname" value=" ' . $in['lname'] . '" required>
+            <input type="text" class="form-control" id="lname" name="lname" value="{$in['lname']}" required>
           </div>
-        </div>' . $aclgrp_buf;
+        </div>
+        $aclgrp_buf
+        HTML;
     }
 }
