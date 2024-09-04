76fcc86a8ca47d66d6824889ff118c01bbe68e9c
Mark Constable <markc@renta.net>
2024-09-04 13:29:33 +1000

    Refactor for PHP 8.3 and remove comments

--- lib/php/plugin.php
+++ lib/php/plugin.php
@@ -1,40 +1,29 @@
 <?php
 
 declare(strict_types=1);
-// lib/php/plugin.php 20150101 - 20230627
+
+// lib/php/plugin.php 20150101 - 20240904
 // Copyright (C) 2015-2023 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Plugin
 {
-    public $g;
-
     public array $inp = [];
-
     protected mixed $dbh = null;
-
     protected string $buf = '';
-
     protected string $tbl = '';
 
-    public function __construct($g)
+    public function __construct(public object $g)
     {
-        $this->g = $g;
-
         $this->inp = util::esc($this->inp);
-
         $o = $this->g->in['o'];
         $m = $this->g->in['m'];
 
-        if (!util::is_usr() && ('auth' !== $o || ('list' !== $m && 'create' !== $m && 'resetpw' !== $m))) {
-            util::redirect($this->g->cfg['self'] . '?o=auth');
+        if (!util::is_usr() && ('auth' !== $o || !in_array($m, ['list', 'create', 'resetpw']))) {
+            util::redirect("{$this->g->cfg['self']}?o=auth");
         }
 
         if ($this->tbl) {
-            if (!is_null($this->dbh)) {
-                db::$dbh = $this->dbh;
-            } elseif (is_null(db::$dbh)) {
-                db::$dbh = new db($this->g->db);
-            }
+            db::$dbh ??= $this->dbh ?? new db($this->g->db);
             db::$tbl = $this->tbl;
         }
 
@@ -48,22 +37,21 @@ class Plugin
 
     public function __call(string $name, array $args): string
     {
-        elog(__METHOD__ . '() name = ' . $name . ', args = ' . var_export($args, true));
-
-        return 'Plugin::' . $name . '() not implemented';
+        elog(__METHOD__ . "() name = $name, args = " . var_export($args, true));
+        return "Plugin::$name() not implemented";
     }
 
     protected function create(): ?string
     {
         if (util::is_post()) {
-            $this->inp['updated'] = date('Y-m-d H:i:s');
-            $this->inp['created'] = date('Y-m-d H:i:s');
+            $this->inp['updated'] = $this->inp['created'] = date('Y-m-d H:i:s');
             $lid = db::create($this->inp);
-            util::log('Item number ' . $lid . ' created', 'success');
+            util::log("Item number $lid created", 'success');
             util::relist();
-        } else {
-            return $this->g->t->create($this->inp);
+            return null;
         }
+        
+        return $this->g->t->create($this->inp);
     }
 
     protected function read(): ?string
@@ -76,13 +64,12 @@ class Plugin
         if (util::is_post()) {
             $this->inp['updated'] = date('Y-m-d H:i:s');
             if (db::update($this->inp, [['id', '=', $this->g->in['i']]])) {
-                util::log('Item number ' . $this->g->in['i'] . ' updated', 'success');
+                util::log("Item number {$this->g->in['i']} updated", 'success');
                 util::relist();
-            } else {
-                util::log('Error updating item.');
+                return null;
             }
+            util::log('Error updating item.');
         }
-
         return $this->read();
     }
 
@@ -90,15 +77,16 @@ class Plugin
     {
         if (util::is_post()) {
             if ($this->g->in['i']) {
-                $res = db::delete([['id', '=', $this->g->in['i']]]);
-                util::log('Item number ' . $this->g->in['i'] . ' removed', 'success');
+                db::delete([['id', '=', $this->g->in['i']]]);
+                util::log("Item number {$this->g->in['i']} removed", 'success');
                 util::relist();
+                return null;
             }
         } else {
             return $this->g->t->delete($this->g->in);
         }
-
         util::log('Error deleting item');
+        return null;
     }
 
     protected function list(): string
