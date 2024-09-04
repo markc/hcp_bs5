031833049c434b5990c3e5fb9a11cb0430598367
Mark Constable <markc@renta.net>
2024-09-04 15:18:15 +1000

    Refactored to PHP 8.3 and removed comments

--- lib/php/plugins/dkim.php
+++ lib/php/plugins/dkim.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-// lib/php/plugins/dkim.php 20180511 - 20230705
-// Copyright (C) 2015-2023 Mark Constable <markc@renta.net> (AGPL-3.0)
+
+// lib/php/plugins/dkim.php 20180511 - 20240904
+// Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Plugins_Dkim extends Plugin
 {
@@ -19,39 +20,35 @@ class Plugins_Dkim extends Plugin
             $domain = escapeshellarg($this->inp['domain']);
             $select = escapeshellarg($this->inp['select']);
             $keylen = escapeshellarg($this->inp['keylen']);
-            util::exe('dkim add ' . $domain . ' ' . $select . ' ' . $keylen);
+            util::exe("dkim add $domain $select $keylen");
         }
-        util::redirect($this->g->cfg['self'] . '?o=' . $this->g->in['o'] . '&m=list');
-        return 'Redirect'; // workaround to satisy string return type
+        util::redirect("{$this->g->cfg['self']}?o={$this->g->in['o']}&m=list");
+        return 'Redirect';
     }
 
     public function read(): string
     {
-        $domain = explode('._domainkey.', $this->inp['dnstxt'])[1]; // too fragile?
+        $domain = explode('._domainkey.', $this->inp['dnstxt'])[1] ?? '';
         $domain_esc = escapeshellarg($domain);
         exec("sudo dkim show {$domain_esc} 2>&1", $retArr, $retVal);
-        $buf = '
-        <b>' . $retArr[0] . '</b><br>
-        <div style="word-break:break-all;font-family:monospace;width:100%;">' . $retArr[1] . '</div>';
-
+        $buf = "<b>{$retArr[0]}</b><br><div style=\"word-break:break-all;font-family:monospace;width:100%;\">{$retArr[1]}</div>";
         return $this->g->t->read(['buf' => $buf, 'domain' => $domain]);
     }
 
     public function update(): string
     {
-        //return $this->list(); // override parent update()
-        util::redirect($this->g->cfg['self'] . '?o=' . $this->g->in['o'] . '&m=list');
-        return "Update"; // workaround to satisy string return type
+        util::redirect("{$this->g->cfg['self']}?o={$this->g->in['o']}&m=list");
+        return "Update";
     }
 
     public function delete(): string
     {
         if (util::is_post()) {
             $domain = escapeshellarg($this->inp['domain']);
-            util::exe('dkim del ' . $domain);
+            util::exe("dkim del $domain");
         }
-        util::redirect($this->g->cfg['self'] . '?o=' . $this->g->in['o'] . '&m=list');
-        return ''; // to make compatible with parent::delete()
+        util::redirect("{$this->g->cfg['self']}?o={$this->g->in['o']}&m=list");
+        return '';
     }
 
     public function list(): string
@@ -59,14 +56,11 @@ class Plugins_Dkim extends Plugin
         $buf = '<p style="columns:350px 3;column-rule: 1px dotted #ddd;text-align:center;">';
         exec('sudo dkim list 2>&1', $retArr, $retVal);
         foreach ($retArr as $line) {
-            if ($retVal === 0) {
-                $buf .= '
-            <a href="?o=dkim&m=read&dnstxt=' . $line . '"><b>' . $line . '</b></a>';
-            } else {
-                $buf .= '<b>' . $line . '</b><br>';
-            }
+            $buf .= match ($retVal) {
+                0 => "<a href=\"?o=dkim&m=read&dnstxt=$line\"><b>$line</b></a>",
+                default => "<b>$line</b><br>",
+            };
         }
-
         return $this->g->t->list(['buf' => $buf . '</p>']);
     }
 }
