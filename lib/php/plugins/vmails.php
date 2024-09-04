57698b95f2eb3c02f190b1158204f2c7b09ca6c6
Mark Constable <markc@renta.net>
2024-09-04 16:16:59 +1000

    Refactored to PHP 8,3 and remove comments

--- lib/php/plugins/vmails.php
+++ lib/php/plugins/vmails.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-// lib/php/plugins/vmails.php 20180826 - 20200414
-// Copyright (C) 2015-2020 Mark Constable <markc@renta.net> (AGPL-3.0)
+
+// lib/php/plugins/vmails.php 20180826 - 20240904
+// Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
 class Plugins_Vmails extends Plugin
 {
@@ -19,14 +20,13 @@ class Plugins_Vmails extends Plugin
     {
         if (util::is_post()) {
             if (!filter_var($this->inp['user'], FILTER_VALIDATE_EMAIL)) {
-                util::log('Email address (' . $this->inp['user'] . ') is invalid');
-                $_POST = [];
-
+                util::log("Email address ({$this->inp['user']}) is invalid");
                 return $this->read();
             }
-            util::exe('addvmail ' . $this->inp['user']);
+            util::exe("addvmail {$this->inp['user']}");
         }
         util::relist();
+        return '';
     }
 
     protected function update(): string
@@ -34,45 +34,48 @@ class Plugins_Vmails extends Plugin
         extract($this->inp);
 
         if ($shpw) {
-            return util::run("shpw {$user}");
+            return util::run("shpw $user");
         }
+
         if ($newpw) {
             return util::run('newpw');
         }
+
         if (util::is_post()) {
             $password = html_entity_decode($password, ENT_QUOTES, 'UTF-8');
             if (util::chkpw($password)) {
-                util::exe("chpw {$user} '{$password}'");
+                util::exe("chpw $user '$password'");
             }
         }
+
         util::relist();
+        return '';
     }
 
     protected function delete(): ?string
     {
         if (util::is_post()) {
-            util::exe('delvmail ' . $this->inp['user']);
+            util::exe("delvmail {$this->inp['user']}");
         }
         util::relist();
+        return null;
     }
 
     protected function list(): string
     {
-        if ('json' === $this->g->in['x']) {
+        if ($this->g->in['x'] === 'json') {
             $columns = [
                 ['dt' => null, 'db' => 'id'],
-                ['dt' => 0, 'db' => 'user', 'formatter' => function ($d, $row) {
-                    return '
-                    <a href="" title="Change password for ' . $d . '" data-id="' . $row['id'] . '" data-user="' . $d . '" data-toggle="modal" data-target="#updatemodal">
-                      <b>' . $d . ' </b></a>';
-                }],
-                ['dt' => 1, 'db' => 'size_mail', 'formatter' => fn ($d) => util::numfmt(intval($d))],
-                ['dt' => 2, 'db' => 'num_total', 'formatter' => fn ($d) => number_format(intval($d))],
-                ['dt' => 3, 'db' => null, 'formatter' => function ($d, $row) {
-                    return '<a href="" title="Remove this Mailbox" data-removeuser="' . $row['user'] . '" data-toggle="modal" data-target="#removemodal">
-                    <small><i class="fas fa-trash fa-fw cursor-pointer text-danger"></i></small>
-                  </a>';
-                }],
+                ['dt' => 0, 'db' => 'user', 'formatter' => fn($d, $row) => "
+                    <a href=\"\" title=\"Change password for $d\" data-id=\"{$row['id']}\" data-user=\"$d\" data-toggle=\"modal\" data-target=\"#updatemodal\">
+                        <b>$d</b>
+                    </a>"],
+                ['dt' => 1, 'db' => 'size_mail', 'formatter' => fn($d) => util::numfmt(intval($d))],
+                ['dt' => 2, 'db' => 'num_total', 'formatter' => fn($d) => number_format(intval($d))],
+                ['dt' => 3, 'db' => null, 'formatter' => fn($d, $row) => "
+                    <a href=\"\" title=\"Remove this Mailbox\" data-removeuser=\"{$row['user']}\" data-toggle=\"modal\" data-target=\"#removemodal\">
+                        <small><i class=\"fas fa-trash fa-fw cursor-pointer text-danger\"></i></small>
+                    </a>"],
                 ['dt' => 4, 'db' => 'updated'],
             ];
 
