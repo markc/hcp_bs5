824f114c0cdf6310c4e21f9122a2e5f3ac3dfd85
Mark Constable <markc@renta.net>
2024-09-07 17:03:06 +1000

    Updated accoring to Claude

--- lib/php/init.php
+++ lib/php/init.php
@@ -3,88 +3,154 @@
 // lib/php/init.php 20150101 - 20240907
 // Copyright (C) 2015-2024 Mark Constable <markc@renta.net> (AGPL-3.0)
 
+declare(strict_types=1);
+
 class Init
 {
-    private $t = null;
+    private Theme $theme;
 
-    public function __construct(object $g)
+    public function __construct(private object $g)
     {
 elog(__METHOD__);
 
-        session_start();
-
-elog('GET=' . var_export($_GET, true));
-elog('POST=' . var_export($_POST, true));
-elog('SESSION=' . var_export($_SESSION, true));
-
-        //$_SESSION = []; // to reset session for testing
-
-        $g->cfg['host'] = $g->cfg['host'] ?? getenv('HOSTNAME');
-
-        util::cfg($g);
-        $g->in = util::esc($g->in);
-        $g->cfg['self'] = str_replace('index.php', '', $_SERVER['PHP_SELF']);
-
-        if (!isset($_SESSION['c'])) $_SESSION['c'] = Util::random_token(32);
-        util::ses('o'); util::ses('m'); util::ses('l');
-        $t = util::ses('t', '', $g->in['t']);
-
-        $t1 = 'themes_' . $t . '_' . $g->in['o'];
-        $t2 = 'themes_' . $t . '_theme';
-
-        $this->t = $thm = class_exists($t1) ? new $t1($g)
-            : (class_exists($t2) ? new $t2($g) : new Theme($g));
-
-        $p  = 'plugins_' . $g->in['o'];
-        if (class_exists($p)) {
-            $g->in['a'] ? util::chkapi($g) : util::remember($g);
-            $g->out['main'] = (string) new $p($thm);
-        } else $g->out['main'] = "Error: no plugin object!";
-
-        if (empty($g->in['x']))
-            foreach ($g->out as $k => $v)
-                $g->out[$k] = method_exists($thm, $k) ? $thm->$k() : $v;
+        $this->initializeSession();
+        $this->configureEnvironment();
+        $this->initializeTheme();
+        $this->loadPlugin();
+        $this->processOutput();
     }
 
     public function __toString(): string
     {
 elog(__METHOD__);
-        $x = $this->g->in['x'];
+
+        $x = $this->g->in['x'] ?? '';
         $out = $this->g->out;
-    
-        if ($x === 'html') {
-            return $out['main'];
-        }
-    
-        if ($x === 'text') {
-            return trim(preg_replace('/^\h*\v+/m', '', strip_tags($out['main'])));
-        }
-    
-        if ($x === 'json' || array_key_exists($x, $out)) {
-            header('Content-Type: application/json');
-            return json_encode($x === 'json' ? $out['main'] : $out[$x], JSON_PRETTY_PRINT);
-        }
-    
-        return $this->g->t->html();
+
+        return match ($x) {
+            'html' => $out['main'],
+            'text' => $this->stripHtmlAndWhitespace($out['main']),
+            'json' => $this->jsonResponse($out['main']),
+            default => $this->theme->html(),
+        };
     }
 
     public function __destruct()
     {
-        elog(__FILE__.' '.$_SERVER['REMOTE_ADDR'].' '.round((microtime(true)-$_SERVER['REQUEST_TIME_FLOAT']), 4)."\n");
+        $executionTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 4);
+        elog("{$_SERVER['REMOTE_ADDR']} - Execution time: {$executionTime}s");
+    }
+
+    private function initializeSession(): void
+    {
+elog(__METHOD__);
+
+        session_start();
+        $_SESSION['c'] ??= Util::random_token(32);
+        $this->logSessionData();
+    }
+
+    private function configureEnvironment(): void
+    {
+elog(__METHOD__);
+
+        $this->g->cfg['host'] ??= getenv('HOSTNAME');
+        util::cfg($this->g);
+        $this->g->in = util::esc($this->g->in);
+        $this->g->cfg['self'] = str_replace('index.php', '', $_SERVER['PHP_SELF']);
+    }
+
+    private function initializeTheme(): void
+    {
+elog(__METHOD__);
+
+        $t = util::ses('t', '', $this->g->in['t']);
+        $themeClass = $this->getThemeClass($t);
+        $this->theme = new $themeClass($this->g);
+    }
+
+    private function loadPlugin(): void
+    {
+elog(__METHOD__);
+
+        $pluginClass = 'plugins_' . $this->g->in['o'];
+        if (class_exists($pluginClass)) {
+            $this->g->in['a'] ? util::chkapi($this->g) : util::remember($this->g);
+            $this->g->out['main'] = (string) new $pluginClass($this->theme);
+        } else {
+            $this->g->out['main'] = "Error: no plugin object!";
+        }
+    }
+
+    private function processOutput(): void
+    {
+elog(__METHOD__);
+
+        if (empty($this->g->in['x'])) {
+            foreach ($this->g->out as $k => $v) {
+                $this->g->out[$k] = method_exists($this->theme, $k) ? $this->theme->$k() : $v;
+            }
+        }
+    }
+
+    private function stripHtmlAndWhitespace(string $content): string
+    {
+elog(__METHOD__);
+
+        return trim(preg_replace('/^\h*\v+/m', '', strip_tags($content)));
+    }
+
+    private function jsonResponse($data): string
+    {
+elog(__METHOD__);
+
+        header('Content-Type: application/json');
+        return json_encode($data, JSON_PRETTY_PRINT);
+    }
+
+    private function getThemeClass(string $theme): string
+    {
+elog(__METHOD__);
+
+        $t1 = 'themes_' . $theme . '_' . $this->g->in['o'];
+        $t2 = 'themes_' . $theme . '_theme';
+        //return class_exists($t1) ? $t1 : (class_exists($t2) ? $t2 : 'Theme');
+        $defaultTheme = Theme::class;
+
+        if (class_exists($t1) && is_subclass_of($t1, Theme::class)) {
+            return $t1;
+        }
+    
+        if (class_exists($t2) && is_subclass_of($t2, Theme::class)) {
+            return $t2;
+        }
+    
+        assert(class_exists($defaultTheme), "Default theme class does not exist");
+        return $defaultTheme;
+    }
+
+    private function logSessionData(): void
+    {
+elog(__METHOD__);
+
+        elog('GET=' . var_export($_GET, true));
+        elog('POST=' . var_export($_POST, true));
+        elog('SESSION=' . var_export($_SESSION, true));
     }
 }
 
-function dbg($var = null)
+function dbg($var = null): void
 {
-    if (is_object($var))
-        error_log(ReflectionObject::export($var, true));
-    ob_start();
-    print_r($var);
-    $ob = ob_get_contents();
-    ob_end_clean();
-    error_log($ob);
+    if (is_object($var)) {
+        $reflection = new ReflectionObject($var);
+        error_log($reflection->getName() . " Object:\n" . print_r($reflection->getProperties(), true));
+    } else {
+        error_log(print_r($var, true));
+    }
 }
-
-function elog(string $content) {
-    if (DBG) error_log($content);
+function elog(string $content): void
+{
+    if (DBG) {
+        error_log($content);
+    }
 }
